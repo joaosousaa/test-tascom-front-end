755fb2a2e5bffcb55b66b0b2cff5ee3f7104eea4
joaosousaa <joaofranciscosouza04@gmail.com>
2024-10-18 00:02:10 -0300

    Fix: error

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import { enUS } from 'date-fns/locale';
 import axios from 'axios';
+import { URL } from '../../utils/constants';
+
 interface Task {
   id: string;
   title: string;
@@ -11,25 +13,27 @@ interface Task {
   created_at: string;
   status: 'CREATED' | 'COMPLETED' | 'CANCELED';
 }
-import { URL } from '../../utils/constants'
+
 const fetchTasks = async (): Promise<Task[]> => {
   const response = await axios.get(`${URL}/api/todo/show`);
-  return response.data.data;
+  return response.data.data; 
 };
 
 const Grid = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   const pollTasks = async () => {
     try {
       const newTasks = await fetchTasks();
       setTasks(newTasks);
+      setError(null); 
     } catch (err: any) {
-      setError(err);
+      setError('Erro ao buscar tarefas.'); 
+    } finally {
+      setIsLoading(false); 
     }
-    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -40,20 +44,18 @@ const Grid = () => {
     return () => clearInterval(intervalId);
   }, []);
 
-
   if (isLoading) {
     return <div className='message'>Loading...</div>;
   }
 
-  if (!Array.isArray(tasks)) {
-    return <div className='message'>No tasks found.</div>;
+  if (error) {
+    return <div className='message'>{error}</div>; 
   }
 
   const formatDateTime = (dateString: string) => {
     const date = new Date(dateString);
     return format(date, "dd MMMM yyyy 'at' hh:mm a", { locale: enUS });
   };
-
 
   const updateTask = async (taskId: string, newStatus: 'COMPLETED' | 'CANCELED') => {
     try {
@@ -64,42 +66,44 @@ const Grid = () => {
       );
       setTasks(updatedTasks);
     } catch (error: any) {
-      console.error('Erro ao atualizar a tarefa:', error.response.data.message);
+      console.error('Erro ao atualizar a tarefa:', error.response?.data?.message || 'Erro desconhecido');
     }
   };
 
   return (
     <main className="custom-frame">
-
-      {tasks.length > 0 ? <>
-        <h1 className="custom-frameTitle">To do</h1>
-
-        <section className='custom-gridSection'>
-          {tasks.map((task) => (
-            <div key={task.id} className="custom-list">
-              <div className="custom-taskCard">
-                <p className='custom-taskName'>{task.title}</p>
-                <p className='custom-taskDescription'>{task.description}</p>
+      {tasks.length > 0 ? (
+        <>
+          <h1 className="custom-frameTitle">To do</h1>
+          <section className='custom-gridSection'>
+            {tasks.map((task) => (
+              <div key={task.id} className="custom-list">
+                <div className="custom-taskCard">
+                  <p className='custom-taskName'>{task.title}</p>
+                  <p className='custom-taskDescription'>{task.description}</p>
+                </div>
+                <div className="custom-cardDateTime">
+                  <p className='custom-dateTime'>{formatDateTime(task.created_at)}</p>
+                </div>
+                <button
+                  className="custom-buttonCompleted"
+                  onClick={() => updateTask(task.id, 'COMPLETED')}
+                >
+                  <img src={check} alt="button to complete task" className='custom-completedIcon' />
+                </button>
+                <button
+                  className="custom-buttonCanceled"
+                  onClick={() => updateTask(task.id, 'CANCELED')}
+                >
+                  <img src={vector} alt="button to cancel task" className='custom-canceledIcon' />
+                </button>
               </div>
-              <div className="custom-cardDateTime">
-                <p className='custom-dateTime'>{formatDateTime(task.created_at)}</p>
-              </div>
-              <button
-                className="custom-buttonCompleted"
-                onClick={() => updateTask(task.id, 'COMPLETED')}
-              >
-                <img src={check} alt="button to complete task" className='custom-completedIcon' />
-              </button>
-              <button
-                className="custom-buttonCanceled"
-                onClick={() => updateTask(task.id, 'CANCELED')}
-              >
-                <img src={vector} alt="button to cancel task" className='custom-canceledIcon' />
-              </button>
-            </div>
-          ))}
-        </section></> : <><div className='message'>No tasks found.</div> </>}
-
+            ))}
+          </section>
+        </>
+      ) : (
+        <div className='message'>No tasks found.</div>
+      )}
     </main>
   );
 };
